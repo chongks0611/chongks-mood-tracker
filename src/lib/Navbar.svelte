5e46737b8d55429764a732e6dceaa220ce4acce6
chongks0611 <veralynchong@gmail.com>
2021-11-28 10:51:06 +0000

    first commit

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,15 @@
+<nav class="navbar">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">My Mood Tracker</a>
+	</div>
+</nav>
+
+<style>
+	nav {
+		background-color: #b983ff;
+	}
+
+	a {
+		color: black;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Greeting from '$lib/Greeting.svelte';
+	import Entry from '$lib/Entry.svelte';
+	import EntryModal from '$lib/EntryModal.svelte';
+</script>
+
+<Greeting />
+
+<!-- Entries -->
+<section class="container px-4 py-3">
+	<div class="d-flex justify-content-between">
+		<div class="p-2">Mood Log</div>
+		<input
+			class="btn btn-light mb-2"
+			data-bs-toggle="modal"
+			data-bs-target="#newEntry"
+			type="button"
+			value="+ New Entry"
+		/>
+	</div>
+
+	<div class="list-group mb-3">
+		<!-- Individual Entries -->
+		<Entry />
+	</div>
+</section>
+
+<EntryModal />
+
+<style>
+	.btn {
+		background-color: #99feff;
+		color: black;
+	}
+</style>

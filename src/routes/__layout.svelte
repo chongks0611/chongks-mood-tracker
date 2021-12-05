402c6a9bb41ed39572fdaf01d72dcb9f50506e5c
chongks0611 <veralynchong@gmail.com>
2021-12-05 10:00:00 +0000

    first commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,32 @@
 <script>
 	import Navbar from '$lib/Navbar.svelte';
+	import { page, session } from '$app/stores';
+    import { browser } from '$app/env';
+    import { goto } from '$app/navigation';
+
+    // code that runs only in the browser
+    if (browser) {
+   	 $session = supabase.auth.session(); // set session
+   	 redirect();
+
+   	 supabase.auth.onAuthStateChange((userSession) => {
+   		 $session = userSession; // set session
+   		 redirect();
+   	 });
+    }
+
+    function redirect() {
+   	 //login redirect
+   	 if ($session && $page.path === '/') {
+   		 goto('/login');
+   	 }
+
+   	 //logout redirect
+   	 if (!$session && $page.path === '/login') {
+   		 goto('/');
+   	 }
+    }
+
 </script>
 
 <Navbar />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,12 @@
 	import Greeting from '$lib/Greeting.svelte';
 	import Entry from '$lib/Entry.svelte';
 	import EntryModal from '$lib/EntryModal.svelte';
+	import supabase from '$lib/db';
+	async function signOut() {
+		const { error } = await supabase.auth.signOut();
+
+		if (error) alert(error.message); // alert if error
+	}
 </script>
 
 <Greeting />
@@ -23,6 +29,11 @@
 		<!-- Individual Entries -->
 		<Entry />
 	</div>
+
+	<!-- Sign Out -->
+	<section class="container px-4 py-3 text-center">
+		<button on:click={signOut} class="btn btn-secondary">Logout</button>
+	</section>
 </section>
 
 <EntryModal />

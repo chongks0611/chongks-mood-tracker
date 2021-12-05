f398667ea362db7e4de9788d780b3a990e46fd05
chongks0611 <veralynchong@gmail.com>
2021-12-05 09:17:23 +0000

    first commit

---
+++ src/lib/db.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL = https://xpfvsytthzghijjlmcts.supabase.co
+  import.meta.env.VITE_SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODY5NTM4MywiZXhwIjoxOTU0MjcxMzgzfQ.gSPflGbxQHht8vbh9n8VhTvrxgwieS0nelrbD5nI4Lw
+)
+
+export default supabase
